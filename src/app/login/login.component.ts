6f8fa2afe978def70241cdb2bc25ffd035d03009
Abdul M Gill <amgill@cloudarchitect.net>
2023-12-18 20:13:17 -0600

    KVR : Angular Binding Example

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,27 +9,28 @@ export class LoginComponent {
       today : Date = new Date();
       title : string = "Login";
       accessCount : number = 2;
-
       imgUrl : string = "https://wallpapers.com/wallpapers/bus-in-sky-animated-cartoon-jrugczle7lyo4zia.html";
       facebookUrl : string = "https://www.facebook.com/";
       twitterUrl : string = "https://twitter.com/";
-
       isDisabled : boolean = false;
+      isFaceBookAcctAvailble : boolean = false;
+      email : string = "abc@gmail.com";
+
 
        getCurrentDate() : Date {
         return new Date();
        }
-
-       isFaceBookAcctAvailble : boolean = false;
 
        getSocialMediaLink() : string {
            if(this.isFaceBookAcctAvailble) {
               return  this.facebookUrl;
            } else {
               return this.twitterUrl;
-           } 
+           }
+       }
 
-
+       updateEmail(newEmail : string) {
+          this.email = newEmail;
        }
  
 }
